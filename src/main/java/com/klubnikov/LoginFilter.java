2a269d4fbee4d7f22ed3422eeb260faca730a725
Alexander Klubnikov <clubman066@gmail.com>
2022-11-12 13:16:50 +0300

    Checking mistakes

--- src/main/java/com/klubnikov/LoginFilter.java
+++ src/main/java/com/klubnikov/LoginFilter.java
@@ -2,6 +2,7 @@ package com.klubnikov;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
@@ -10,11 +11,13 @@ import java.io.IOException;
 public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String userNameFromForm = (String) servletRequest.getServletContext().getAttribute("loginFromFrom");
-        String passwordFromForm = (String) servletRequest.getServletContext().getAttribute("passFromForm");
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String userNameFromSession = (String) req.getSession().getAttribute("loginFromSession");
+        String passwordFromSession = (String) req.getSession().getAttribute("passFromSession");
 
-        if (userNameFromForm!=null && passwordFromForm!=null) {
+
+        if (userNameFromSession!=null && passwordFromSession!=null) {
             servletRequest.getRequestDispatcher("/success.html").forward(servletRequest, servletResponse);
         } else resp.sendError(403, "Oops, you entered wrong username or password");
     }
--- src/main/java/com/klubnikov/LoginServlet.java
+++ src/main/java/com/klubnikov/LoginServlet.java
@@ -8,17 +8,30 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/login")
+//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+//    @Override
+//    public void init() throws ServletException {
+//        getServletContext().setAttribute("init-login", "alex");
+//        getServletContext().setAttribute("init-pass", "123");
+//    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         String login = req.getParameter("login");
         String password = req.getParameter("password");
-        if (login.equals("alex") && password.equals("123")) {
-            req.getServletContext().setAttribute("loginFromFrom", login);
-            req.getServletContext().setAttribute("passFromForm", password);
+
+//        String initLogin = (String) getServletContext().getAttribute("init-login");
+//        String initPass = (String) getServletContext().getAttribute("init-pass");
+
+        String initLogin = getServletConfig().getInitParameter("init-login");
+        String initPass = getServletConfig().getInitParameter("init-pass");
+
+        if (login.equals(initLogin) && password.equals(initPass)) {
+            req.getSession().setAttribute("loginFromSession", login);
+            req.getSession().setAttribute("passFromSession", password);
         }
         resp.sendRedirect("/success.html");
     }
